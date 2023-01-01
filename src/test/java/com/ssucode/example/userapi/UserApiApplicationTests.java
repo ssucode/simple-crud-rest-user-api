d1536ba3fc4333fd0b89c692ad9dab59372a62c7
ssucode <ssuya79@gmail.com>
2023-01-01 23:35:03 +0900

    build:init

---
+++ src/test/java/com/ssucode/example/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssucode.example.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
